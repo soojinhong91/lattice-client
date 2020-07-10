51cbf221d11ec03b64e48cba30b8c4e93251cb84
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-07-10 18:57:15 +1000

    basic setting done

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Lattice from './Lattice';
+
+function App() {
+  return (
+    <div className="App">
+      <Lattice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Lattice.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Lattice extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Lattice coming soon</h1>
+      </div>
+    );
+  }
+}
+
+export default Lattice;
