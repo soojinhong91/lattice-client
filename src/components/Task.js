fde1d0cd28e83ae3d3f440d430d843bd72ce450b
Jonathan C MacMillan <jmacmill@gmail.com>
2020-07-12 13:48:31 +1000

    small changes to task component

---
+++ src/components/Task.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class Task extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Task coming soon</h1>
+      </div>
+    );
+  }
+}
+
+export default Task;
+
+
+//make an axios request to the db for all of the tasks
+//iterate over all of the tasks on the json page
+//map the tasks to their own unique paragraphs
+
+//somehow, you have to allow for the creation of new 
