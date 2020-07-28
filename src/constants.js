468f69b966eb88066b35f91542d2078e7a7ee686
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-07-28 20:49:18 +1000

    server url to heroku

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const server_url = 'http://localhost:3000/'
-// const server_url = 'https://lattice-server.herokuapp.com/'
+// const server_url = 'http://localhost:3000/'
+const server_url = 'https://lattice-server.herokuapp.com/'
 
 const server = path => server_url + path + '/'
 
